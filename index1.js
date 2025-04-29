257b9b4597e1a239af18cc40439bed32ed00a492
islose <islosdark@gmail.com>
2025-04-29 19:05:13 +0100

    upgrade 1.2

--- index1.js
+++ index1.js
@@ -38,6 +38,14 @@ useCases.addEventListener("mouseleave", () => {
   gsap.to(dropdown2, { autoAlpha: 0, y: 10, duration: 0.3});
 });
 
+gsap.to(".floating-image", {
+  y: -20,
+  repeat: -1,
+  yoyo: true,
+  ease: "power1.inOut",
+  duration: 2
+});
+
 const ytVid = document.getElementById("ytVid");
 
 gsap.registerPlugin(ScrollTrigger);
@@ -45,12 +53,11 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.from("#ytVid", {
   opacity: 0,
   y: 200,
-  duration: 1,
+  duration: 0.6,
   ease: "power2.out",
   scrollTrigger: {
       trigger: "#ytVid",
-      start: "top 90%",
-      end: "top 50%",
+      start: "top 80%",
       once: true,
   }
 });
@@ -60,11 +67,11 @@ const omenAmd = document.getElementById("#omenAmd");
 gsap.from(".omenAmd", {
   opacity: 0,
   y: 200,
-  duration: 1,
+  duration: 0.6,
   ease: "power2.out",
   scrollTrigger: {
     trigger: ".omenAmd",
-    start: "top 90%",
+    start: "top 75%",
     end: "top 50%",
     once: true,
   }
@@ -102,7 +109,7 @@ document.addEventListener("DOMContentLoaded", function () {
 gsap.from(".feedBacks", {
   opacity: 0,
   y: 200,
-  duration: 1,
+  duration: 0.6,
   ease: "power2.out",
   scrollTrigger: {
     trigger: ".feedBacks",
@@ -115,11 +122,11 @@ gsap.from(".feedBacks", {
 gsap.from(".h2-part", {
   opacity: 0,
   y: 200,
-  duration: 1,
+  duration: 0.6,
   ease: "power2.out",
   scrollTrigger: {
     trigger: ".partner",
-    start: "top 50%",
+    start: "top 80%",
     end: "top 10%",
     once: true,
   }
@@ -128,7 +135,7 @@ gsap.from(".h2-part", {
 gsap.from(".logo-partner", {
   opacity: 0,
   y: 200,
-  duration: 1,
+  duration: 0.6,
   ease: "power2.out",
   scrollTrigger: {
     trigger: ".partner",
@@ -144,35 +151,150 @@ let index = 0;
 gsap.set(images[0], { opacity: 1});
 
 function changeImage() {
-  gsap.to(images[index], { opacity: 0, duration: 2});
-  index = (index +1) % images.length;
+  gsap.to(images[index], { opacity: 0, duration: 1.5});
+  index = (index + 1) % images.length;
   gsap.to(images[index], { opacity: 1, duration: 2});
 }
 
 setInterval(changeImage, 3000);
 
-gsap.from(".app-info", {
+gsap.from("#app-info1", {
   opacity: 0,
   y: 200,
-  duration: 1,
+  duration: 0.6,
   ease: "power2.out",
   scrollTrigger: {
-    trigger: ".app-info",
-    start: "top 50%",
+    trigger: "#app-info1",
+    start: "top 80%",
     end: "top 10%",
     once: true,
   }
 });
 
-gsap.from(".app-info-reverse", {
+gsap.from("#app-info2", {
   opacity: 0,
   y: 200,
-  duration: 1,
+  duration: 0.6,
   ease: "power2.out",
   scrollTrigger: {
-    trigger: ".app-info-reverse",
-    start: "top 50%",
+    trigger: "#app-info2",
+    start: "top 80%",
     end: "top 10%",
     once: true,
   }
+});
+
+gsap.from("#app-info3", {
+  opacity: 0,
+  y: 200,
+  duration: 0.6,
+  ease: "power2.out",
+  scrollTrigger: {
+    trigger: "#app-info3",
+    start: "top 80%",
+    end: "top 10%",
+    once: true,
+  }
+});
+
+gsap.from(".kad-tech h2", {
+  opacity: 0,
+  y: 200,
+  duration: 0.6,
+  ease: "power2.out",
+  scrollTrigger: {
+    trigger: ".kad-tech h2",
+    start: "top 80%",
+    end: "top 10%",
+    once: true,
+  }
+});
+
+gsap.from(".kad-tech p", {
+  opacity: 0,
+  y: 200,
+  duration: 0.6,
+  ease: "power2.out",
+  scrollTrigger: {
+    trigger: ".kad-tech p",
+    start: "top 80%",
+    end: "top 10%",
+    once: true,
+  }
+});
+
+gsap.from(".guide-boxes", {
+  opacity: 0,
+  y: 200,
+  duration: 0.6,
+  ease: "power2.out",
+  scrollTrigger: {
+    trigger: ".guide-boxes",
+    start: "top 80%",
+    once: true,
+  }
+});
+
+gsap.from(".dow-title", {
+  opacity: 0,
+  y: 40,
+  duration: 0.6,
+  ease: "power2.out",
+  scrollTrigger: {
+    trigger: ".dow-title",
+    start: "top 60%",
+    once: true,
+  }
+});
+
+
+
+gsap.from(".faq h2", {
+  opacity: 0,
+  y: 40,
+  duration: 0.6,
+  ease: "power2.out",
+  scrollTrigger: {
+    trigger: ".faq h2",
+    start: "top 80%",
+    once: true,
+  }
+});
+
+
+
+const questions = document.querySelectorAll(".faq-question");
+
+questions.forEach(question => {
+  question.addEventListener("click", () => {
+    const faqItem = question.parentElement;
+    const answer = faqItem.querySelector(".faq-answer");
+    const arrow = question.querySelector(".arrow");
+
+    const isOpen = answer.style.maxHeight;
+
+    if(isOpen) {
+      answer.style.maxHeight = null;
+      arrow.style.transform = "rotate(0deg)";
+    }
+
+    else {
+      answer.style.maxHeight = answer.scrollHeight + "px";
+      arrow.style.transform = "rotate(180deg)";
+    }
+  });
+});
+
+document.querySelectorAll(".faq-item").forEach((item) => {
+  gsap.from(item, {
+    y: 40,
+    opacity: 0,
+    duration: 0.6,
+    ease: "power2.out",
+    scrollTrigger: {
+      trigger: item,
+      start: "top 90%",
+      toggleActions: "play none none none"
+    }
+  });
 });
